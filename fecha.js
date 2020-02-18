02ef139230578e0ecd52fc8eed542bf3d22ecf86
20161012 LEONARDO YAHEL ISORDIA AVILA <20161012@TELEMATICA.UCOL.MX>
2020-02-17 18:51:56 -0600

    Paciente finish

--- fecha.js
+++ fecha.js
@@ -3,14 +3,16 @@ export default class Fecha {
      {
          this.fecha = fecha
          this.fechaActual = new Date()
-         this.años = Math.floor(((fechaActual - this.fecha) / (1000 * 60 * 60 * 24) / 365));
+         this.años = Math.floor(((this.fechaActual - this.fecha) / (1000 * 60 * 60 * 24) / 365));
      }
 
      getAños(){return this.años}
      getMeses(){return (this.años*12)}
      getSemanas(){return (this.años*12*4)}
      getDias(){return (this.años*12*4*7)}
-     getFecha(){return (`${this.fecha.getDay()}/${this.fecha.getMonth()}/${this.fecha.getFullYear()}`)}
+     getFecha(){
+         return (`${this.fecha.getDate()}/${this.fecha.getMonth()}/${this.fecha.getFullYear()}`)
+        }
      getDiaFecha(){
         let dia = this.fecha.getDay()
         let semana = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import Nombre from "./nombre.js"
 import Doctor from "./doctor.js"
+import Paciente from "./paciente.js"
+import Fecha from "./fecha.js"
 
 class Main{
 
     constructor(){
         this.testPepe = new Nombre("pepe","jorge","rodrigo")
         this.testDoct = new Doctor(this.testPepe,"master","3124573641","cedulaxD")
+        this.paciente = new Paciente(this.testPepe,new Fecha(new Date(2002,5,20)),"312-526-2456")
     }
 
     testNombre(){
@@ -19,8 +22,13 @@ class Main{
     testDoc(){
         console.log(this.testDoct.getPerfil())
     }
+
+    testPac(){
+        console.log(this.paciente.getPerfil())
+    }
 }
 
 let test = new Main();
 test.testNombre();
 test.testDoc();
+test.testPac();
